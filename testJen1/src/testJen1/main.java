50f9c406612cb4e8de828d181d56fcc70511f5f5
lobnaelsammak <38563000+lobnaelsammak@users.noreply.github.com>
2021-03-28 11:28:38 +0200

    Add files via upload

---
+++ testJen1/src/testJen1/main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package testJen1;
+
+/**
+ * @author lelsammak
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello!");
+	}
+
+}
